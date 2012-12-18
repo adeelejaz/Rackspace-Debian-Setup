d077e33610c34eccc81f3ed5e8b191fbebb7d9fc
Adeel Ejaz <adeel.e@gmail.com>
2012-12-18 16:19:37 +0000

    Firewall

--- 1_firewall.sh
+++ 1_firewall.sh
@@ -3,3 +3,39 @@
 echo ""
 echo "*** Setup Firewall"
 echo ""
+
+# Read config file
+. ./setup.cfg
+
+echo "* Creating rules file"
+
+cat <<EOF > /etc/iptables.rules
+*filter
+
+-A INPUT -i lo -j ACCEPT
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A OUTPUT -j ACCEPT
+
+-A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+-A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+-A INPUT -p tcp -m state --state NEW -m tcp --dport $ssh_port -j ACCEPT
+
+-A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+
+-A INPUT -j REJECT
+-A FORWARD -j REJECT
+
+COMMIT
+EOF
+
+echo "* Creating Boot Script for Firewall"
+
+cat <<EOF > /etc/network/if-pre-up.d/iptables
+#!/bin/sh
+
+iptables-restore < /etc/iptables.rules
+EOF
+
+chmod +x /etc/network/if-pre-up.d/iptables
+
+echo ""
